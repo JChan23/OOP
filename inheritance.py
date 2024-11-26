f7498222ae1bbfc9f64b095de9e39f788e14c821
JChan23 <143363241+JChan23@users.noreply.github.com>
2024-11-26 21:32:00 +0800

    Create inheritance.py

---
+++ inheritance.py
@@ -0,0 +1,34 @@
+class Employee:
+    #self.__HourlyPay REAL
+    #self.__EmployeeNumber STRING
+    #self.__JobTitle STRING
+
+    def __init__(self, pay, ID, title):
+        self.__HourlyPay = pay
+        self.__EmployeeNumber = ID
+        self.__JobTitle = title
+        self.__PayYear2022 = [] #array of 52 elements of type REAL
+        for i in range(52):
+            self.__PayYear2022.append(0.00)
+
+    def GetEmployeeNumber(self):
+        return self.__EmployeeNumber
+
+    def SetPay(self, WeekNum, HoursWorked):
+        TotalPay = self.__HourlyPay*HoursWorked
+        self.__PayYear2022[WeekNum-1] = TotalPay
+
+    def GetTotalPay(self):
+        return sum(self.__PayYear2022)
+
+
+class Manager(Employee):
+    #self.__BonusValue REAL
+
+    def __init__(self, pay, ID, title, bonus):
+        super().__init__(pay, ID, title)
+        self.__BonusValue = bonus
+
+    def SetPay(self, WeekNum, HoursWorked):
+        HoursWorked = HoursWorked*(1+(self.__BonusValue/100))
+        super().SetPay(WeekNum, HoursWorked)

008c8320bedd5e299a661085f56445a28aa4ca0e
JChan23 <143363241+JChan23@users.noreply.github.com>
2025-03-02 19:51:43 +0800

    Create LinkedList.py

---
+++ LinkedList.py
@@ -0,0 +1,54 @@
+#OOP based linked list
+class Node:
+    def __init__(self, data):
+        self.data = data
+        self.next = None
+
+class LinkedList:
+    def __init__(self):
+        self.head = None
+
+    def insert_to_end(self, data):
+        new_node = Node(data)
+        if self.head is None:
+            self.head = new_node
+            return
+        last = self.head
+        while last.next: #is not none
+            last = last.next
+        last.next = new_node
+
+    def insert_to_start(self, data):
+        new_node = Node(data)
+        new_node.next = self.head
+        self.head = new_node
+
+    def delete(self, key):
+        temp = self.head
+        if temp == key and temp.data:
+            self.head = temp.next
+            temp = None
+            return
+        prev = None
+        while temp and temp.data != key:
+            prev = temp
+            temp = temp.next
+        if temp is None:
+            return
+        prev.next = temp.next
+        temp = None
+
+    def search(self, key):
+        current = self.head
+        while current:
+            if current.data == key:
+                return True
+            current = current.next
+        return False
+
+    def display(self):
+        current = self.head
+        while current:
+            print(current.data, end=" -> ")
+            current = current.next
+        print("None")

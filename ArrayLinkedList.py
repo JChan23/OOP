07b755c8e1a45ce8ade31a05b654a387fa6974a2
JChan23 <143363241+JChan23@users.noreply.github.com>
2025-03-02 20:17:17 +0800

    Create ArrayLinkedList.py

---
+++ ArrayLinkedList.py
@@ -0,0 +1,62 @@
+Array = [['', i + 1] for i in range(9)] + [['', -1]]  # 10 slots, linked as free list
+startPointer = -1
+freePointer = 0
+
+def insert(item):
+    global startPointer
+    global freePointer
+
+    if freePointer == -1:
+        print("List is full")
+        return
+
+    new_index = freePointer
+    freePointer = Array[new_index][1]
+
+    current = startPointer
+    prev = -1
+    while current != -1 and Array[current][0] < item:
+        prev = current
+        current = Array[current][1]
+
+    Array[new_index] = [item, current]
+
+    if prev == -1:
+        startPointer = new_index
+    else:
+        Array[prev][1] = new_index
+
+def delete(item):
+    global startPointer
+    global freePointer
+
+    if startPointer == -1:
+        print("The list is empty")
+        return False
+
+    current = startPointer
+    prev = -1
+    while current != -1 and Array[current][0] < item:
+        prev = current
+        current = Array[current][1]
+
+    if current == -1 or Array[current][0] != item:
+        return False
+
+    if prev == -1:
+        startPointer = Array[current][1]
+    else:
+        Array[prev][1] = Array[current][1]
+
+    Array[current] = ['', freePointer]
+    freePointer = current
+    return True
+
+def search(item):
+    current = startPointer
+    while current != -1:
+        if Array[current][0] == item:
+            return current
+        current = Array[current][1]
+    return -1
+
